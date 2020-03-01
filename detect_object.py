9417e65d7252302e9bb0c6cd58d894f6f11019b1
OzzyWL <ozwilson@gmail.com>
2020-03-01 18:11:25 +0100

    Update detect_object.py

--- detect_object.py
+++ detect_object.py
@@ -11,7 +11,7 @@ base_model = tf.keras.applications.MobileNetV2(input_shape=IMG_SHAPE,
                                                include_top=False,
                                                weights='imagenet')
 
-dummy_mat = np.zeros((1, 160, 160, 3), dtype=np.float64)
+dummy_mat = np.zeros((1, 160, 160, 3), dtype=np.float32)
 feature_batch = base_model(dummy_mat)
 global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
 feature_batch_average = global_average_layer(feature_batch)
